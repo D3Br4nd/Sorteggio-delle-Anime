d8078edef55e4dd89334950f34664c4bb9fcea11
D3Br4nd <cesare.brandini@gmail.com>
2026-01-22 20:42:52 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ export default function App() {
     }, []);
 
     return (
-        <div className="relative min-h-screen min-h-dvh overflow-hidden no-select">
+        <div className="relative min-h-dvh overflow-hidden no-select">
             {/* Branding Footer */}
             <div className="fixed bottom-0 left-0 right-0 z-50 flex justify-between items-center px-6 py-4 pointer-events-none">
                 <motion.img
